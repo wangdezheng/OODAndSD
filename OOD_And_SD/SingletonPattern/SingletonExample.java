e86ee0fe8c091f63c7bdb653a30b9e678e879ddb
Tom <wang.dezheng@uwlax.edu>
2017-11-07 22:41:27 -0800

    update Singleton example

--- OOD_And_SD/SingletonPattern/SingletonExample.java
+++ OOD_And_SD/SingletonPattern/SingletonExample.java
@@ -5,30 +5,33 @@ package SingletonPattern;
  * If the static method might be called from multiple threads simultaneously, measures may need to be taken to prevent race conditions that could result in the creation of multiple instances of the class.
  */
 
-public final class SingletonExample {
-    private static volatile SingletonExample instance = null;
-
-    private SingletonExample() {
-    }
-
-    public static SingletonExample getInstance() {
-        if (instance == null) {
-            synchronized (SingletonExample.class) {
-                if (instance == null) {
-                    instance = new SingletonExample();
-                }
-            }
-        }
-        return instance;
-    }
-}
-    // Simple implementation of singleton (single thread environment)
 //public final class SingletonExample {
-//    private static final SingletonExample INSTANCE = new SingletonExample();
+//    private static volatile SingletonExample instance = null;
 //
-//    private SingletonExample() {}
+//    private SingletonExample() {
+//    }
 //
 //    public static SingletonExample getInstance() {
-//        return INSTANCE;
+//        if (instance == null) {
+//            synchronized (SingletonExample.class) {
+//                if (instance == null) {
+//                    instance = new SingletonExample();
+//                }
+//            }
+//        }
+//        return instance;
 //    }
 //}
+    // Simple implementation of singleton (single thread environment)
+public final class SingletonExample {
+    private static SingletonExample INSTANCE = null;
+
+    private SingletonExample() {}
+
+    public static SingletonExample getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new SingletonExample();
+        }
+        return INSTANCE;
+    }
+}
